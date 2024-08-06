0fe2d3111c5d28bf94122653f09231958a3c8e55
Wemerson Jose da Cunha <josedacunhawemerson@gmail.com>
2024-08-06 20:30:20 +0000

    Adicionando página de detalhe de vaga

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, jsonify, render_template
-from database import carrega_vagas_db
+from flask import Flask, json, jsonify, render_template
+from database import carrega_vagas_db, carrega_vaga_db
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     vagas = carrega_vagas_db()
     return render_template("home.html", vagas=vagas)
 
+
 @app.route("/vagas")
 def lista_vagas():
     vagas = carrega_vagas_db()
     return jsonify(vagas)
-    
+
+
+@app.route("/vaga/<id>")
+def mostra_vaga(id):
+    vaga = carrega_vaga_db(id)
+    if not vaga:
+        return "Not Found", 404
+    return render_template("detalhevaga.html", vaga=vaga)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -15,3 +15,15 @@ def carrega_vagas_db():
         for vaga in resultado.all():
             vagas.append(vaga._asdict())
         return vagas
+
+def carrega_vaga_db(id):
+    with engine.connect() as conn:
+        resultado = conn.execute(text("SELECT * FROM vagas WHERE id = :val"),
+            {"val": id})
+        registro = resultado.mappings().all()
+        if len(registro) == 0:
+            return None
+        else:
+            return dict(registro[0])
+        
+        
